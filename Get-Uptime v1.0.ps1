e45ba4fbc2c6ca5ab0792406e178c5cbe5fe12b6
lars halvorsen <32477535+larsinus@users.noreply.github.com>
2019-05-22 22:34:49 +0200

    Update Get-Uptime v1.0.ps1
    Added logic to compare the two keys to verify if they match instead of having to manually compare them...

--- Get-Uptime v1.0.ps1
+++ Get-Uptime v1.0.ps1
@@ -1,3 +1,41 @@
 
-# Get-Uptime
-(get-date) - (Get-CimInstance Win32_OperatingSystem).LastBootUpTime
+
+# functions
+function Get-IniContent ($filePath){
+    $ini = @{}
+    switch -regex -file $FilePath
+    {
+        “^\[(.+)\]” # Section
+        {
+            $section = $matches[1]
+            $ini[$section] = @{}
+            $CommentCount = 0
+        }
+        “^(;.*)$” # Comment
+        {
+            $value = $matches[1]
+            $CommentCount = $CommentCount + 1
+            $name = “Comment” + $CommentCount
+            $ini[$section][$name] = $value
+        } 
+        “(.+?)\s*=(.*)” # Key
+        {
+            $name,$value = $matches[1..2]
+            $ini[$section][$name] = $value
+        }
+    }
+    return $ini
+}
+
+# The SMSPublicRootKey should match...
+$MobileClientTcf = Get-IniContent("\\CM\E$\Microsoft Configuration Manager\bin\i386\MobileClient.tcf")
+$serverSide = ($MobileClientTcf["Site"].'   SMSPublicRootKey').Trim()
+#Get-Content -Path \\CM\E$\Microsoft Configuration Manager\bin\i386\MobileClient.tcf
+
+# ...the TrustedRootKey
+$clientSide = (Get-WmiObject -Namespace root\ccm\locationservices -Class TrustedRootKey).TrustedRootKey
+
+if ($clientSide -eq $serverSide){
+    Write-Host -ForegroundColor Green "SMSPublicRootKey is verified against the client's TrustedRootKey"
+    Write-Host $serverSide
+}
