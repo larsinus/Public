9351c7cdae7fffe5e725dbf6d8246620ca77c2d8
lars halvorsen <32477535+larsinus@users.noreply.github.com>
2020-03-24 08:31:10 -0700

    Add files via upload

---
+++ Configuration Manager/Add-DevicesToCollection.ps1
@@ -0,0 +1,23 @@
+﻿
+#@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+
+# Tell Powershell to ignore any errors that may fill up the screen.
+$ErrorActionPreference = 'silentlycontinue'
+
+##########################################################################
+#VARIABLES
+##########################################################################
+$TextFile = <Path To TextFile>
+$CollectionID = <ID of Collection>
+##########################################################################
+
+$ComputerList = Get-Content $TextFile #text file contains only server names
+
+Foreach($Computer in $ComputerList)
+{
+	Write-Host $Computer -Foreground green -Background Black
+	Add-CMDeviceCollectionDirectMembershipRule -CollectionID $CollectionID -ResourceId (Get-CMDevice -Name $Computer).ResourceID
+}
+
+Write-Host "Complete" -Foreground magenta -Background black
+Write-Host ""
