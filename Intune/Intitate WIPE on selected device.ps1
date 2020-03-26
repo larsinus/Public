358e57643e5b4e6cbac169dad7658e5a1312f72d
lars halvorsen <32477535+larsinus@users.noreply.github.com>
2020-03-26 15:41:44 +0100

    Add files via upload
    uploaded Initiate SYNC and Initiate WIPE scripts

---
+++ Intune/Intitate WIPE on selected device.ps1
@@ -0,0 +1,34 @@
+﻿
+$ModuleStatus = Get-Module -Name Microsoft.Graph.Intune
+if ($null -eq $ModuleStatus) {
+    Install-Module -Name Microsoft.Graph.Intune -RequiredVersion 6.1902.1.
+}
+
+If ($null -eq $creds){
+    $creds = Get-Credential
+    Connect-MSGraph -Credential $creds
+}
+
+$colDevices = @{}
+$colDevices = Get-IntuneManagedDevice
+Write-Host "*** Device List ***"
+Write-Host "-------------------"
+Foreach ($device in $colDevices){
+    Write-Host -ForegroundColor Cyan $device.deviceName
+}
+$selected = Read-Host "which device do you want to wipe? (type the name)"
+if ($selected -in $colDevices.devicename) {
+    Write-Host -ForegroundColor Red "WARNING! " -NoNewline
+    Write-Host "Are you sure you want to " -NoNewline
+    Write-Host -ForegroundColor Red "WIPE " -NoNewline
+    Write-Host "the computer labeled " -NoNewline
+    Write-Host -ForegroundColor Cyan ($selected + " ") -NoNewline
+    $wipeVerification = Read-Host "(Y/N)"
+    if ($wipeVerification -ieq "y") {
+        Invoke-DeviceManagement_ManagedDevices_Wipe -managedDeviceId (($colDevices | Where-Object {$_.devicename -eq $selected}).id)
+        Write-Host -ForegroundColor Green ("INFO - Initiated WIPE on " + $selected)
+    }
+}
+else {
+    Write-Host -ForegroundColor Red ("ERROR! Couldn't find device with name " + $selected)
+}
